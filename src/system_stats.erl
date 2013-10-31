ebf9deb5169bb4e1395ecb80fb9162a5fac58aa4
Louis-Philippe Gauthier <lpgauth@gmail.com>
2013-10-31 11:15:55 -0400

    Initial commit (code)

---
+++ src/system_stats.erl
@@ -0,0 +1,57 @@
+-module(system_stats).
+-include("system_stats.hrl").
+
+-export([
+    proc_cpuinfo/1,
+    proc_load_avg/1,
+    proc_pid_stat/1,
+    proc_pid_stat/2,
+    proc_stat/1
+]).
+
+%% public
+proc_cpuinfo(_) -> % #stats {} = _Stats) ->
+    {ok, CpuInfo} = system_stats_utils:read_file("/proc/cpuinfo"),
+    ok = file:close(File),
+    io:format("~p~n", [CpuInfo]).
+
+proc_pid_stat(#stats {} = Stats) ->
+    proc_pid_stat(os:getpid(), Stats).
+
+proc_pid_stat(Pid, #stats {} = Stats) ->
+    Filename = "/proc/" ++ Pid ++ "/stat",
+    {ok, File} = file:open(Filename, [read, raw]),
+    {ok, ProcStat} = file:read(File, 400),
+    ok = file:close(File),
+    {ok, [_, _, _, _, _, _, _, _, _, _, _, _, _, Utime, Stime, Cutime, Cstime,
+        _, _, _, _, _, Vsize, Rss], _} = io_lib:fread("~d ~s ~c ~d ~d ~d ~d
+        ~d ~u ~u ~u ~u ~u ~u ~u ~d ~d ~d ~d ~d ~d ~u ~u ~d", ProcStat),
+    Stats#stats {
+        cpu_cstime = Cstime,
+        cpu_cutime = Cutime,
+        cpu_stime = Stime,
+        cpu_utime = Utime,
+        mem_rss = Rss,
+        mem_vsize = Vsize
+    }.
+
+proc_load_avg(#stats {} = Stats) ->
+    {ok, File} = file:open("/proc/loadavg", [read, raw]),
+    {ok, LoadAvg} = file:read(File, 40),
+    ok = file:close(File),
+    {ok, [Load1, Load5, Load15], _} = io_lib:fread("~f ~f ~f", LoadAvg),
+    Stats#stats {
+        load_1 = Load1,
+        load_5 = Load5,
+        load_15 = Load15
+    }.
+
+proc_stat(#stats {} = Stats) ->
+    {ok, File} = file:open("/proc/stat", [read, raw]),
+    {ok, ProcStat} = file:read(File, 100),
+    ok = file:close(File),
+    {ok, Times, _} = io_lib:fread("cpu ~u ~u ~u ~u ~u ~u ~u ~u ~u ~u", ProcStat),
+    CpuTotal = lists:foldl(fun(X, Sum) -> X + Sum end, 0, Times),
+    Stats#stats {
+        cpu_total = CpuTotal
+    }.
---
+++ src/system_stats_utils.erl
@@ -0,0 +1,46 @@
+-module(system_stats_utils).
+-include("system_stats.hrl").
+
+-export([
+    cpu_percent/2,
+    read_file/1
+]).
+
+cpu_percent(#stats {
+        cpu_cstime = Cstime,
+        cpu_cutime = Cutime,
+        cpu_stime = Stime,
+        cpu_total = Total,
+        cpu_utime = Utime
+    }, #stats {
+        cpu_cstime = Cstime2,
+        cpu_cutime = Cutime2,
+        cpu_stime = Stime2,
+        cpu_total = Total2,
+        cpu_utime = Utime2
+    }) ->
+
+    TotalDiff = Total2 - Total,
+    Ucpu = 100 * (((Utime2 + Cutime2) - (Utime + Cutime)) / TotalDiff),
+    Scpu = 100 * (((Stime2 + Cstime2) - (Stime + Cstime)) / TotalDiff),
+    {Ucpu, Scpu}.
+
+
+read_file(Filename) ->
+    case file:open(Filename, [read, raw]) of
+        {ok, File} ->
+            read(File, []);
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+read(File, Acc) ->
+    case file:read(File, 4096) of
+        {ok, Data} ->
+            read(File, [Data | Acc]);
+        {error, Reason} ->
+            {error, Reason};
+        eof ->
+            [Data] = Acc,
+            {ok, Data}
+    end.
